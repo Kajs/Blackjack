9f5cdfac4ee163524c31c80386b7b3634fb48435
Cornelius (scra) <cornelius.a.r@gmail.com>
2026-09-24 18:53:21 +0200

    Tilføjet kommentarer til Dealer_server
    Tilføjet kommentarer til funktioner, samt gjort det lidt mere tydeligt hvilken funktion der udskriver til terminalen, i tilfælde af fejl.

--- Dealer_server.py
+++ Dealer_server.py
@@ -7,6 +7,7 @@ clients = {}
 validActions = ["ACTION: HIT", "ACTION: STAND", "ACTION: QUIT"]
 playerNumber = 0
 
+#Wait for a response from a player client and return it
 def getMessage(playerNumber):
     global serverSocket
     global clients  
@@ -17,20 +18,23 @@ def getMessage(playerNumber):
         decodedMessage = message.decode()
         #print(playerNumberToName(playerNumber) + " says:", decodedMessage)
         return decodedMessage
-    else: print("Error: playerNumber does not exist in clients.")        
+    else: print("Error in getMessage: playerNumber does not exist in clients.")        
 
+#Send a text command to a player client
 def sendMessage(playerNumber, message):
     global clients
 
     if playerNumber in clients:
         connection, address = clients[playerNumber]
         connection.sendall(message.encode())
-    else: print("Error: playerNumber does not exist in clients.") 
+    else: print("Error in sendMessage: playerNumber does not exist in clients.") 
 
+#Get a string version of a playername from the players number
 def playerNumberToName(playerNumber):
     playerName = "PLAYER" + str(playerNumber)
     return playerName
 
+#Tell a player client what number it's been assigned on the server side
 def sendPlayerName(playerNumber):
     global clients
 
@@ -38,7 +42,7 @@ def sendPlayerName(playerNumber):
         playerName = playerNumberToName(playerNumber)
         message = "YOURNAMEIS: " + playerName + '\n'
         sendMessage(playerNumber, message)
-    else: print("Error: playerNumber does not exist in clients.") 
+    else: print("Error in sendPlayerName: playerNumber does not exist in clients.") 
 
 def startServer(host, port):
     global serverSocket
@@ -51,19 +55,20 @@ def startServer(host, port):
 def closeServer():
     global serverSocket
     if serverSocket == None:
-        print("Error: Server socket has not been started.")
+        print("Error in closeServer: server socket has not been started.")
     else:
         print("Closing server socket.")
         serverSocket.close()
         print("Server socket has been closed.")
 
+#Wait for a player to connect, assign the player a number and send it to the player client
 def acceptConnection():
     global clients
     global serverSocket
     global playerNumber
 
     if serverSocket == None:
-        print("Error: Server socket has not been started")
+        print("Error in acceptConnection: server socket has not been started")
     else:
         print("Waiting for player to connect...")
         connection, address = serverSocket.accept()
@@ -74,6 +79,7 @@ def acceptConnection():
         return playerNumber
     return -1
 
+#End the connection to a specific player
 def closeConnection(playerNumber):
     global clients
 
@@ -82,34 +88,25 @@ def closeConnection(playerNumber):
         connection.close()
         del clients[playerNumber]
         print(playerNumberToName(playerNumber) + " has been disconnected.")
-    else: print("Error: playerNumber does not exist in clients.")  
+    else: print("Error in closeConnection: playerNumber does not exist in clients.")  
 
  
-
+#Send an a request for the player to take an action (such as hit or stand), to the players client
 def requestAction(playerNumber):
     global validActions
     sendMessage(playerNumber, "REQUEST: ACTION\n")
     action = getMessage(playerNumber)
     if action in validActions: return action
     else:
-        print("Dealer_server: ERROR: Invalid action:", action)
+        print("ERROR in requestAction: invalid action:", action)
         return ("ERROR: Invalid action: " + str(action))
 
-def requestClose(playerNumber):
-    sendMessage(playerNumber, "REQUEST: CLOSE\n")
+#Tell the player client to close it's connection and gui
+def requestClose(playerNumber): sendMessage(playerNumber, "REQUEST: CLOSE\n")
 
+#Send the contents of the dealers hand and the players hand to the player client in text format 
 def updatePlayerBoard(playerNumber, dealerHand, playerHand):
     message = "UPDATE:"
     message += "DEALER:" + dealerHand + ';'
     message += playerNumberToName(playerNumber) + ':' + playerHand + '\n'
     sendMessage(playerNumber, message)
-    
-
-
-#startServer(serverAddress, serverPort)
-#acceptConnection()
-#sendMessage(0, "Hello there Player 1.")
-#requestAction(0)
-#getMessage(0)
-#closeConnection(0)
-#closeServer()
